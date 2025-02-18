83171224f0f2315f4d9efde32ff513a9e55c124f
Alexander Dorozhko <47350603+wafxzy@users.noreply.github.com>
2025-02-18 15:05:09 +0100

    added auth structure

---
+++ CommonReg_Ang/src/app/main-page/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CommonReg-main-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss',
+})
+export class MainPageComponent {}
